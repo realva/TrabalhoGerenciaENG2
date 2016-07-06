e7a24938bbfc98c34c31d96cbf4eefaac70489a3
Renato Alvarenga <otaner.alvarenga@gmail.com>
2016-07-06 15:57:32 -0300

    TrabalhoGerencia1.0.0
    Versão 1.0

---
+++ A.java
@@ -0,0 +1,22 @@
+/**
+ * @author Renato
+ *
+ */
+public class A {
+
+	private int A1;
+	private float A2;
+	
+	public A() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
