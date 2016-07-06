61a01eca0d3af0e142b77641525b0d5ee7f3bb6f
Renato Alvarenga <otaner.alvarenga@gmail.com>
2016-07-06 17:36:58 -0300

    Versão1.3

---
+++ B.java
@@ -0,0 +1,43 @@
+/**
+ * @author Renato
+ *
+ */
+public class B {
+
+	private int B1;
+	private float B2;
+	
+	public A() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void setB1(int pB1){
+		
+		this.B1 = pB1;
+		
+	}
+	
+	public int getB1(){
+		
+		return this.B1;
+		
+	}
+	
+	public void setB2(float pB2){
+		
+		this.B2 = pB2;
+		
+	}
+	
+	public float getB2(){
+		
+		return this.B2;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
